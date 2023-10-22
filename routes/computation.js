e7337466658f8ddab9ef620e3268e4bb9aab807e
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2023-10-22 15:19:24 -0500

    BONUS

---
+++ routes/computation.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Computation page. */
+router.get('/', function(req, res, next) {
+let x= req.query.x?req.query.x:Math.random();
+let y=Math.cos(x);
+    res.send(`Math.cos() applied to ${x} is ${y}`);
+});
+
+module.exports = router;
