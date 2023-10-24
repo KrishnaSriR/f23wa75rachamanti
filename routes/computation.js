c7104ac4c609b924ee5c703cb5ee95949d59605c
KrishnaSriR <KrishnaSriR@users.noreply.github.com>
2023-10-23 23:56:05 -0500

    More changes

--- routes/computation.js
+++ routes/computation.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Computation page. */
 router.get('/', function(req, res, next) {
-let x= req.query.x?req.query.x:Math.random();
+let x= req.query.x?req.query.x:Math.round(Math.random() * 100 + 1);
 let y=Math.cos(x);
     res.send(`Math.cos() applied to ${x} is ${y}`);
 });
